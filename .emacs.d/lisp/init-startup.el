4eee2dfcfad19d6acf779b996af5379ee202a150
iicce <nhjk5523@outlook.com>
2026-03-20 20:41:52 +0800

    Update init-startup.el

--- .emacs.d/lisp/init-startup.el
+++ .emacs.d/lisp/init-startup.el
@@ -1,7 +1,10 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(global-hl-line-mode 1)
+
 (setq inhibit-startup-screen t)
+(set-face-foreground 'hl-line nil)
 (add-to-list 'default-frame-alist '(undecorated . t))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -13,6 +16,7 @@
 (setq display-line-numbers-current-absolute t
       display-line-numbers-width 3
       display-line-numbers-minimum-width 3)
+
 
 (setq-default indent-tabs-mode nil
               tab-width 4)
@@ -27,7 +31,10 @@
 (set-face-attribute 'fixed-pitch nil :font "JetbrainsMono Nerd Font")
 (set-face-attribute 'variable-pitch nil :font "JetbrainsMono Nerd Font")
 
-(add-hook 'prog-mode-hook 'whitespace-mode)
+; (add-hook 'prog-mode-hook 'whitespace-mode)
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 (provide 'init-startup)
