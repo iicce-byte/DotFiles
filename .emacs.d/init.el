683625e39ca0727e8ed91c55be5ecfa13b31b617
iicce <nhjk5523@outlook.com>
2026-03-20 20:47:56 +0800

    Refactor init.el by removing unnecessary requires
    Removed redundant package and function requirements.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,19 +2,9 @@
 
 
 (require 'init-startup)
-(require 'init-elpa)
-(require 'init-themes)
-(require 'init-packages)
 (require 'init-functions)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'init-elpa)
+
+(require 'init-themes)
+(require 'init-evil)
+(require 'init-lsp)
